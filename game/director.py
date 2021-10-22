97bba3eddec3ed70d8465a82301b64e25bd9a081
sharedordaz <sharedordaz@live.com.mx>
2021-10-21 22:16:49 -0500

    Finished for today

--- game/director.py
+++ game/director.py
@@ -7,22 +7,35 @@ class Director:
     def __init__(self):
         self.console = Console()
         self.board = Board()
+        self.calculator = Calculator()
     def start_game(self):
-        pass
+        self.create_players()
+        self.print_game()
+        self.assign_numbers()
+        self.turns()
+
     def create_players(self):
-        player1_name = self.console.input_whatever("Enter a name for player 1: ")
-        player2_name = self.console.input_whatever("Enter a name for player 2: ")
-        player3_name = self.console.input_whatever("Enter a name for player 3: ")
+        player1_name = input("Enter a name for player 1: ")
+        player2_name = input("Enter a name for player 2: ")
+        player3_name = input("Enter a name for player 3: ")
         self.player1 = Player(player1_name)
         self.player2 = Player(player2_name)
         self.player3 = Player(player3_name)
 
-    def game(self):
+    def assign_numbers(self):
+        self.player1.number = self.board.get_number()
+        self.player2.number = self.board.get_number()
+        self.player3.number = self.board.get_number()
 
-        info1 = self.board.get_info(self.player1)
-        info2 = self.board.get_info(self.player2)
-        info3 = self.board.get_info(self.player3)
-        
+
+    def print_game(self):
+
+        info1 = self.board.get_info(self.player1.name, self.player1.number, self.player1.info)
+        info2 = self.board.get_info(self.player2.name, self.player2.number, self.player2.info)
+        info3 = self.board.get_info(self.player3.name, self.player3.number, self.player3.info)
         self.console.print_info(info1, info2, info3)
 
-
+    def turns(self):
+        self.player1.ask_number(self.player1.name)
+        self.player1.info = self.calculator.game_info(self.player1.number, self.player1.guess)
+        self.print_game()
--- game/player.py
+++ game/player.py
@@ -1,14 +1,16 @@
 class Player:
     """Guess the numbers that you need to guess"""
-    pass
-    def __init__(self, name):
+    def __init__(self, name, number=0, guess=0, info="****"):
         self.name = name
-    def ask_number(self):
-        guess = int(input("What is your guess? "))
-        while guess > 9999:
+        self.number = number
+        self.guess = guess
+        self.info = info
+    def ask_number(self, user):
+        guess = int(input(f"{user}: What is your guess? "))
+        if guess > 9999:
             print("Please write a number of only four digits\n")
-            self.ask_number()    
-        return guess
+            self.ask_number()
+        self.guess = guess
 
     def numToList(self, guess):
         guess_list = []
