0f270c0c74d95119ce6b69d783d5bc35cd7f52ac
sharedordaz <sharedordaz@live.com.mx>
2021-10-21 21:44:11 -0500

    ?

--- game/calculator.py
+++ game/calculator.py
@@ -7,4 +7,14 @@ class Calculator:
         ast_list = ["*","*","*","*"]
         return ast_list
 
-    
+    def game_info(self, number, guessed_number):
+        info = ["*","*","*","*"]
+        for x in range(4):
+            if number[x-1] == guessed_number[x-1]:
+                info[x-1] = ("x")
+            elif  (number[x-1] != guessed_number[x-1]) and (guessed_number[x-1] in number):
+                info[x-1] = ("o")
+            else:
+                info[x-1] = ("*")
+
+        return info
--- game/console.py
+++ game/console.py
@@ -4,9 +4,15 @@ class Console:
     def __init__(self):
         pass
 
-    def print_info(self,info):
-        print("--------------------")
-        print(info)
-        print("--------------------")
+    def print_info(self,info1, info2, info3):
+        print("\n--------------------")
+        print(info1)
+        print(info2)
+        print(info3)
+        print("--------------------\n")
+
     def print_whatever(self, whatever):
         print(whatever)
+
+    def input_whatever(self, whatever):
+        input(whatever)
