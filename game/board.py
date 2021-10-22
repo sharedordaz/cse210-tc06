ad236d97f433c34a63c066d4bf56599e8d02f003
sharedordaz <sharedordaz@live.com.mx>
2021-10-21 20:40:25 -0500

    created player3

--- game/board.py
+++ game/board.py
@@ -3,3 +3,5 @@ class Board:
     pass
     def __init__(self):
         pass
+    def get_info(self, p1_name, p2_name, p3_name):
+        return (f"--------------------\nPlayer {p1_name}: ----, ****\nPlayer {p2_name}: ----, ****\nPlayer {p3_name}: ----, ****\n--------------------\n")
--- game/director.py
+++ game/director.py
@@ -2,6 +2,7 @@ from game.board import Board
 from game.player import Player
 from game.console import Console
 from game.calculator import Calculator
+
 class Director:
     pass
     def __init__(self):
@@ -11,6 +12,7 @@ class Director:
     def create_players(self):
         player1_name = input("Enter a name for player 1: ")
         player2_name = input("Enter a name for player 2: ")
+        player2_name = input("Enter a name for player 3: ")
         player1 = Player(player1_name)
         player2 = Player(player2_name)
-
+        player3 = Player(player3_name)
